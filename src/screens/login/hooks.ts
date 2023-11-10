2538f92ed6a10501060a4f0c344b142fafdbca2f
Jayesh Purohit <jayeshpurohit456789@gmail.com>
2023-11-10 12:38:22 +0530

    onMutate added

--- src/screens/login/hooks.ts
+++ src/screens/login/hooks.ts
@@ -5,6 +5,9 @@ export const useAddLogin = () => {
   const {mutate, isLoading, data, error, isError} = useMutation({
     mutationKey: 'addLogin',
     mutationFn: ({email, password}: IAddLogin) => addLogin({email, password}),
+    onMutate: async ({email, password}: IAddLogin) => {
+      console.log(email, password, 'onMutate...');
+    },
   });
 
   return {mutate, isLoading, data, error, isError};
